6421f040db68789c50cc4c5c0dc8bdd432f28651
msaisanjayvarma <msaisanjayvarma@gmail.com>
2023-04-19 06:25:21 +0530

    Dom project

---
+++ task3/dom.js
@@ -0,0 +1,4 @@
+document.getElementById('main-header').style.borderBottom = 'solid 3px #000';
+let title = document.getElementsByClassName('title');
+title[0].innerHTML='<b>Add Items</b>';
+title[0].style.color='green';
