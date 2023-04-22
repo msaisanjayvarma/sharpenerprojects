61e5629093465fc9224b0f3386c9ff0b27a3ace3
msaisanjayvarma <msaisanjayvarma@gmail.com>
2023-04-22 15:52:05 +0530

    new commit task6

---
+++ task6/dom5(1).js
@@ -0,0 +1,7 @@
+let liq=document.querySelectorAll('li');
+liq[2].style.color='green';
+let liqo=document.querySelectorAll('li:nth-child(odd)');
+
+for(let i=0;i<liqo.length;i++){
+    liqo[i].style.backgroundColor='green';
+}
---
+++ task6/dom5.js
@@ -0,0 +1,5 @@
+let liq = document.querySelector('li:nth-child(2)');
+liq.style.backgroundColor='green';
+let liqv = document.querySelector('li:nth-child(3)');
+liqv.style.visibility = "hidden";
+
